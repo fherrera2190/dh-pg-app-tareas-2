db3bda7364f56c223443a736019272b7a908a03f
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-05-19 03:28:29 -0300

    Se Termino la funcion modificar y borrar

--- app.js
+++ app.js
@@ -3,21 +3,47 @@ const moduloFuncionesDeTareas = require('./modulos/funcionesDeTareas.js')
 const argv = require('process').argv;
 require('colors');
 const accion = argv[2] === undefined ? undefined : argv[2].toLowerCase();
+
 switch (accion) {
+
+    //Lista todas las tareas existente con el comando:
+    //node app listart
     case 'listar':
         console.log('------------------\nListado de tareas\n------------------'.green);
         moduloFuncionesDeTareas.listar();
         console.log();
         break;
+    //Crea una tarea con el comando:
+    //node app crear titulo
     case 'crear':
         console.log(moduloFuncionesDeTareas.crear(argv[3]));
         break;
+    //Muestras todas las tareas con el estado pasado como argumento
+    //node app filtrar estado
     case 'filtrar':
         moduloFuncionesDeTareas.filtrarPorEstado(argv[3]);
-
         break;
+    //Modifica el estado de una tarea con el comando :
+    //node app modificar numeroroTarea nuevoEstado
     case 'modificar':
+        if (+argv[3] < 1) {
+            console.log("El no existe una terea en esa posicion".red)
+            break;
+        }
+        if (argv[4] === undefined) {
+            console.log("Error: no ingreso el nuevo estado de la tarea".red)
+            break;
+        }
         moduloFuncionesDeTareas.modificarEstado(+argv[3], argv[4]);
+        break;
+    //Elimina una tarea con el comando :
+    //node app borrar numeroDeTarea 
+    case 'borrar':
+        if (+argv[3] < 1) {
+            console.log("El no existe una terea en esa posicion".red)
+            break;
+        }
+        moduloFuncionesDeTareas.borrarTarea(+argv[3] - 1);
         break;
     case (undefined):
         console.log('------------------------------------\nAtención - Tienes que pasarme una acción\nLas acciones disponibles son: listar,crear, filtrar\n----------------------------------------'.yellow);
